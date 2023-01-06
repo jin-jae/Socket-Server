cd49244aded296672137b2231389311eccec53de
KIM JINJAE <jinjae.dev@gmail.com>
2023-01-06 13:10:53 +0900

    fix: add console message

--- server.c
+++ server.c
@@ -5,7 +5,9 @@
 #include "library.h"
 
 void    Server(char* port) {
-    ft_print(port);ft_print("\n");
+    ft_print("--------------- Server Program made by KIM JINJAE ---------------\n");
+    ft_print("Server will try to open port number "); ft_print(port); ft_print("...\n");
+
     int serverSocket;
     int clientSocket;
 
@@ -19,32 +21,42 @@ void    Server(char* port) {
      *  protocol -
      *  written from "man socket" from linux System Calls Manual
     */
+    ft_print("Setting server socket... ");
     serverSocket = socket(PF_INET, SOCK_STREAM, 0);
     if (serverSocket == -1)
         throwException("SOCK_ERR");
+    ft_print("done!\n");
 
+    ft_print("Setting server address... ");
     memset(&serverAddress, 0, sizeof(serverAddress));
     /* IPV4 체계를 이용해 통신합니다. */
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
     serverAddress.sin_port = htons(ft_atoi(port));
+    ft_print("done!\n");
 
+    ft_print("Bind server address... ");
     if (bind(serverSocket, (struct sockaddr*) &serverAddress, sizeof(serverAddress)) == -1)
         throwException("BIND_ERR");
+    ft_print("done!\n");
+    ft_print("Listen to port... ");
     if (listen(serverSocket, 5) == -1)
         throwException("LISTEN_ERR");
 
-//    while (1)
-//    {
+    while (1)
+    {
+        ft_print("Trying to accept client socket... ");
         clientAddressSize = sizeof(clientAddress);
         clientSocket = accept(serverSocket, (struct sockaddr*) &clientAddress, &clientAddressSize);
         if (clientSocket == -1)
         {
             throwException("ACCEPT_ERR");
-//            break ;
+            break ;
         }
+        ft_print("done!\n");
         checkRequest(clientSocket);
-//    }
+    }
     close(clientSocket);
     close(serverSocket);
+    ft_print("------------------------- Server ended --------------------------\n");
 }
