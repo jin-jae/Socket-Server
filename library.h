d127f45a0a19b9b52ca377520e6b1734ccee8424
KIM JINJAE <jinjae.dev@gmail.com>
2023-01-08 20:21:07 +0900

    add: comment

--- library.h
+++ library.h
@@ -5,18 +5,22 @@
 #ifndef SERVER_CLIENT_FT_LIBRARY_H
 #define SERVER_CLIENT_FT_LIBRARY_H
 
+// read, write 등 저수준 함수를 포함하는 header 입니다.
 #include <unistd.h>
+// printf, 파일 입출력을 지원하는 header 입니다.
 #include <stdio.h>
+// socket 을 지원하는 header 입니다.
 #include <sys/socket.h>
+// inet_addr 함수를 이용해 IPv4 주소 조작을 하도록 지원하는 header 입니다.
 #include <arpa/inet.h>
+// strcmp, strcat 등 문자열 처리를 지원하는 header 입니다.
 #include <string.h>
 
 #include "exception.h"
 #include "server.h"
 #include "request.h"
 
-int ft_atoi(char* str);
-void    ft_char(char c);
-void	ft_print(char *str);
+// 버퍼의 최대 사이즈는 1024로 임의로 지정하였습니다.
+#define BUFFER_SIZE 1024
 
 #endif //SERVER_CLIENT_FT_LIBRARY_H
